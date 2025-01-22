a29c40410e38a2204d939e56f7d27b235af754a0
Leon <mail@leonkuehn.de>
2025-01-22 15:20:03 +0100

    Button hinzugefuegt

--- app/src/main/java/com/example/udemyapp/MainActivity.java
+++ app/src/main/java/com/example/udemyapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.udemyapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void clickButton(View view){
+
+        Log.i("info", "Button CLOOOCKKKKED");
     }
 }
